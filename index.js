82df92040aa08ca156e7f785d07f1158dfdf14e6
mhhasanmehedi <mmmehedi53@gmail.com>
2022-10-06 17:50:59 +0600

    create reducer for counter

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 // defining constants
 const INCREMENT = "INCREMENT";
 const DECREMENT = "DECREMENT";
-const ADD_USER = "ADD_USER";
 
 // state
 const initialCounterState = {
   count: 0,
-};
-
-const initialUsersState = {
-  users: [{ name: "Anisul islam" }],
 };
 
 // action --object- I>> type, II>> payload
@@ -21,7 +16,25 @@ const decrementCounter = () => {
   return { type: DECREMENT };
 };
 
-const addUser = () => {
-  return { type: ADD_USER, payload: { name: "Shakil" } };
+// create reducer for counter
+const counterReducer = (state = initialCounterState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    default:
+      state;
+  }
 };
 
+// 1. state
+// 2. dispatch action
+// 3. reducer
+// 4. store
