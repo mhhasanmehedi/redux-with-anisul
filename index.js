09fbe6e1c0091ad8e4c7d5a35f503286ebd63666
mhhasanmehedi <mmmehedi53@gmail.com>
2022-10-09 18:51:15 +0600

    fetch data using redux thunk

--- index.js
+++ index.js
@@ -1,54 +1,99 @@
+// async actions - api calling
+// api url - https://jsonplaceholder.typicode.com/todos
+// middleware - redux-thunk
+// axios api
+
+const axios = require("axios");
 const { createStore, applyMiddleware } = require("redux");
-const { default: logger } = require("redux-logger");
+const { default: thunk } = require("redux-thunk");
 
-// products constants
-const GET_PRODUCTS = "GET_PRODUCTS";
-const ADD_PRODUCT = "ADD_PRODUCT";
+// constants
+const GET_TODOS_REQUEST = "GET_TODOS_REQUEST";
+const GET_TODOS_SUCCESS = "GET_TODOS_SUCCESS";
+const GET_TODOS_FAILED = "GET_TODOS_FAILED";
+const API_URL = "https://jsonplaceholder.typicode.com/todos";
 
-// initial state for product
-const initialProductState = {
-  products: ["Sugar", "salt"],
-  numberOfProducts: 2,
+// states
+const initialTodosState = {
+  todos: [],
+  isLoading: false,
+  error: null,
 };
 
-// Product actions
-const getProducts = () => {
+// actions
+const getTodosRequest = () => {
   return {
-    type: GET_PRODUCTS,
+    type: GET_TODOS_REQUEST,
   };
 };
 
-const addProduct = (product) => {
+const getTodosSuccess = (todos) => {
   return {
-    type: ADD_PRODUCT,
-    payload: product,
+    type: GET_TODOS_SUCCESS,
+    payload: todos,
   };
 };
 
-// product reducer
-const productReducer = (state = initialProductState, action) => {
+const getTodosFailed = (error) => {
+  return {
+    type: GET_TODOS_FAILED,
+    payload: error,
+  };
+};
+
+// reducers
+const todosReducer = (state = initialTodosState, action) => {
   switch (action.type) {
-    case GET_PRODUCTS:
+    case GET_TODOS_REQUEST:
       return {
         ...state,
+        isLoading: true,
       };
 
-    case ADD_PRODUCT:
+    case GET_TODOS_SUCCESS:
       return {
-        products: [...state.products, action.payload],
-        numberOfProducts: state.numberOfProducts + 1,
+        ...state,
+        isLoading: false,
+        todos: action.payload,
       };
+
+    case GET_TODOS_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
+
     default:
-      return state;
+      return {
+        state,
+      };
   }
 };
 
+// async action creator
+const fetchData = () => {
+  return (dispatch) => {
+    dispatch(getTodosRequest());
+    axios
+      .get(API_URL)
+      .then((res) => {
+        const todos = res.data;
+        const titles = todos.map((todo) => todo.title);
+        dispatch(getTodosSuccess(titles));
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        dispatch(getTodosFailed(errorMessage));
+      });
+  };
+};
+
 // store
-const store = createStore(productReducer, applyMiddleware(logger));
+const store = createStore(todosReducer, applyMiddleware(thunk));
 
 store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch(getProducts());
-store.dispatch(addProduct("Potato"));
+store.dispatch(fetchData());
